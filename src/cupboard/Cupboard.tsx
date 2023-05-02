0446a2a51eca583f49d0fe6a86a5ad6a4648c5aa
Robert Clayburn <rob@infosum.com>
2023-05-02 23:24:05 +0100

    add linting - update deps

--- src/cupboard/Cupboard.tsx
+++ src/cupboard/Cupboard.tsx
@@ -1,49 +1,43 @@
-import React, { FC } from 'react';
-import {
-  Actor,
-  State,
-} from 'xstate';
-
-import { useActor } from '@xstate/react';
-
-import {
-  CupboardContext,
-  CupboardEvent,
-} from './cupboard.machine';
+import { GameContext } from '../App';
 import { PickedUpItem } from './PickedUpItem';
 
-interface IProps {
-  actor: Actor<State<CupboardContext, CupboardEvent>, any>;
-}
-export const Cupboard: FC<IProps> = ({
-  actor,
-}) => {
-  const [state, send] = useActor(actor);
-
-  const { item, cupboard } = state.context as CupboardContext;
+export const Cupboard = () => {
+  const actor = GameContext.useSelector(({ context }) => context.windows.cupboard.actor);
+  if (!actor) {
+    return null;
+  }
+  const { send } = actor;
+  const context = actor.getSnapshot()?.context;
+  const matches = actor.getSnapshot()?.matches;
+  if (!context || !matches) {
+    return null;
+  }
+  const { item, cupboard } = context;
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
       <div style={{ flexGrow: 1 }}>
         <h2>Cupboard</h2>
         {
-          state.matches('warnFull') && <div role="alert">No more room</div>
+          matches('warnFull') && <div role="alert">No more room</div>
         }
         <div style={{ display: 'flex' }}>
           <div style={{ width: '10rem' }}>
             <PickedUpItem
               item={item}
-              send={send} />
+              send={send}
+            />
           </div>
           <div>
             <h3>In cupboard</h3>
             {
               cupboard.items.map((i) => <div key={i.title}>
                 {i.img &&
-                  <img src={i.img}
+                  <img
+                    src={i.img}
                     alt={i.title}
-                    style={{ width: '32px', height: '32px' }} />
-                }
+                    style={{ width: '32px', height: '32px' }}
+                  />}
                 {i.title}
                 <button onClick={() => send({ type: 'REMOVE_ITEM', item: i })}>X</button>
               </div>)
@@ -53,5 +47,5 @@ export const Cupboard: FC<IProps> = ({
       </div>
 
     </div>
-  )
-}
+  );
+};
